b0f736b04b10b57e3621e503021a1a96b3de3417
at-rooshee <ru_sha_nia@yahoo.ca>
2017-07-14 23:22:17 +0300

    Fixing toggling

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,81 +1,83 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class Test extends Component {
-  
-  
-  componentWillReceiveProps() {
-    console.log('receive')
-  }
-  shouldComponentUpdate() {
-    console.log('shouldUpdate')
-    return true
-  }
-  componentWillUpdate() {
-    console.log('willUpdate')
-  }
-  componentDidUpdate() {
-    console.log ('didUpdate')
-  }
 
+  // componentWillReceiveProps() {
+  //   console.log('receive')
+  // }
+  // shouldComponentUpdate() {
+  //   console.log('shouldUpdate')
+  //   return true
+  // }
+  // componentWillUpdate() {
+  //   console.log('willUpdate')
+  // }
+  // componentDidUpdate() {
+  //   console.log ('didUpdate')
+  // }
 
   render() {
     console.log('render Test')
     return (
       <div className="App">
-          <h1>Welcome</h1>
-        </div>
+        <h1>Welcome</h1>
+      </div>
     );
   }
 }
 
-
 class App extends Component {
-  constructor() {
-    super(); 
-    this.state = {isToggledOn: true};
-      console.log('constructor')
-      this.handleClick = this.handleClick.bind(this);
-    }
-    
-    componentWillMount(){
-      console.log('willMount')
-    }
-    
-    componentDidMount(){
-      console.log('didMount')
-    }
-    
-    _changeName = () => {
-      this.setState({
-        name: 'Peter'
-      })
-    }
-    
-    handleClick() {
-      this.setState(prevState => ({
-        isToggledOn: !prevState.isToggledOn
-      }));
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  // componentWillMount(){
+  //   console.log('willMount')
+  // }
+  //
+  // componentDidMount(){
+  //   console.log('didMount')
+  // }
+  //
+  // _changeName = () => {
+  //   this.setState({
+  //     name: 'Peter'
+  //   })
+  // }
 
   render() {
-    console.log('privet render')
+    // console.log('privet render')
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" onClick={()=>this._changeName()} />
+          <img src={logo} className="App-logo" alt="logo" 
+            // onClick={() => this._changeName()}}
+          />
           <button onClick={this.handleClick}>
-           {this.state.isToggleOn ? 'ON' : 'OFF'}
+            {this.state.isToggleOn
+              ? 'ON'
+              : 'OFF'}
           </button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Test name={this.state.name}/>
       </div>
     );
   }
+
 }
 
 export default App;
